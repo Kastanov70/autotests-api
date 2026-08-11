f25817eb6ca43027a096e0321e8622431c870895
Kastanov70 <76150267+Kastanov70@users.noreply.github.com>
2026-08-11 23:02:50 +0300

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 from pydantic import BaseModel, EmailStr
 
 from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import get_private_users_client, PrivateUsersClient
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 # Импортируем запрос и ответ создания пользователя, модель данных пользователя
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema
@@ -20,21 +22,31 @@ class UserFixture(BaseModel):
     def password(self) -> str:  # Быстрый доступ к password пользователя
         return self.request.password
 
+    # добавили новое свойство authentication_user
+    @property
+    def authentication_user(self):
+        return AuthenticationUserSchema(email=self.request.email, password=self.request.password)
 
+
+# Используем фикстуру authentication_client, которая создает нужный API клиент
 @pytest.fixture
 def authentication_client() -> AuthenticationClient:
     return get_authentication_client()
 
-
+# Используем фикстуру public_users_client, которая создает нужный API клиент
 @pytest.fixture
 def public_users_client() -> PublicUsersClient:
     return get_public_users_client()
 
-
 # Фикстура для создания пользователя
 @pytest.fixture
-# Используем фикстуру public_users_client, которая создает нужный API клиент
 def function_user(public_users_client: PublicUsersClient) -> UserFixture:
     request = CreateUserRequestSchema()
     response = public_users_client.create_user(request)
     return UserFixture(request=request, response=response)  # Возвращаем все нужные данные
+
+
+# Используем фикстуру private_users_client, которая создает нужный API клиент
+@pytest.fixture
+def private_users_client(function_user: UserFixture) -> PrivateUsersClient:
+    return get_private_users_client(user=function_user.authentication_user)
