284dc8c4dfa63d6699d3d425dd1d2b82c353fc1f
Kastanov70 <76150267+Kastanov70@users.noreply.github.com>
2026-08-20 09:21:13 +0300

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Обновление задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -4,11 +4,12 @@ import pytest
 
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
-    GetExerciseResponseSchema
+    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
+    assert_update_exercise_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -45,6 +46,27 @@ class TestExercises:
         assert_status_code(response.status_code, HTTPStatus.OK)
         # проверяем ответ
         assert_get_exercise_response(function_exercise.response, response_data)
+
+        # Проверяем соответствие JSON-ответа схеме
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_update_exercise(
+            self,
+            exercises_client: ExercisesClient,
+            function_exercise:ExerciseFixture
+    ):
+        # Отправляем запрос на обновление задания
+        request = UpdateExerciseRequestSchema()
+        response = exercises_client.update_exercise_api(
+            exercise_id=function_exercise.response.exercise.id,
+            request=request
+        )
+        response_data = UpdateExerciseResponseSchema.model_validate_json(response.text)
+
+        # Проверяем статус код
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        # проверяем ответ
+        assert_update_exercise_response(request=request, response=response_data)
 
         # Проверяем соответствие JSON-ответа схеме
         validate_json_schema(response.json(), response_data.model_json_schema())
