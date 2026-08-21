585a1b057462128e307ee56d7039896bd4eb0b9c
Kastanov70 <76150267+Kastanov70@users.noreply.github.com>
2026-08-21 17:19:24 +0300

    Интеграция Allure в автотесты

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -1,11 +1,16 @@
 from http import HTTPStatus
 
 import pytest
+import allure
+from allure_commons.types import Severity
 
 from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from fixtures.users import UserFixture
+from tools.allure.epics import AllureEpic
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 # Импортируем функцию для проверки ответа создания юзера
@@ -15,12 +20,18 @@ from tools.fakers import fake
 
 @pytest.mark.users
 @pytest.mark.regression
+@allure.tag(AllureTag.USERS, AllureTag.REGRESSION)
+@allure.epic(AllureEpic.LMS)
 class TestUsers:
     @pytest.mark.parametrize("email", [
         "mail.ru",
         "gmail.com",
         "example.com"
     ])
+    @allure.tag(AllureTag.CREATE_ENTITY)
+    @allure.title("Create user")
+    @allure.story(AllureStory.CREATE_ENTITY)
+    @allure.severity(Severity.BLOCKER)
     def test_create_user(self, email: str, public_users_client: PublicUsersClient):
         request = CreateUserRequestSchema(email=fake.email(domain=email))
         response = public_users_client.create_user_api(request)
@@ -32,6 +43,10 @@ class TestUsers:
 
         validate_json_schema(response.json(), response_data.model_json_schema())
 
+    @allure.tag(AllureTag.GET_ENTITY)
+    @allure.story(AllureStory.GET_ENTITY)
+    @allure.title("Get user me")
+    @allure.severity(Severity.CRITICAL)
     def test_get_user_me(
             self,
             private_users_client: PrivateUsersClient,
